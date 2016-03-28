ca90eb7c24da11e9b02da319fcb26d82da1d7a7b
Rivalo <rivalo@home.nl>
2016-03-29 00:05:38 +0200

    Some basic stuff

--- src/Game.cpp
+++ src/Game.cpp
@@ -14,18 +14,17 @@ Game::Game(std::vector<std::vector<int>> map) : UI(map) {
 
 void Game::Reset(){
 	GameObjectStruct pacman;
-		pacman.x = 1;
-		pacman.y = 1;
-		pacman.type = PACMAN;
-		pacman.dir = UP;
+	pacman.x = 1;
+	pacman.y = 1;
+	pacman.type = PACMAN;
+	pacman.dir = UP;
 
-		this->setScore(0); /* <-- Put your code in the setter */
+	this->setScore(0);
 
-				// Set the amount of lives
-		this->setLives(3); /* <-- Put your code in the setter */
+	this->setLives(3);
 
-				// Render the scene
-		std::vector<GameObjectStruct> objects = {pacman};
-		this->update(objects); /* <-- Put your code in the update function (e.g. game->getStructs()) */
+	// Render the scene
+	std::vector<GameObjectStruct> objects = {pacman};
+	this->update(objects);
 }
 
